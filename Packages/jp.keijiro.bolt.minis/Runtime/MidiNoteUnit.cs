ca0a7df9d845f2fcbba47941eb39f2b0f85a5ad1
Keijiro Takahashi <keijiro@gmail.com>
2020-11-05 13:53:23 +0900

    Refactoring to MidiNoteUnit

--- Packages/jp.keijiro.bolt.minis/Runtime/MidiNoteUnit.cs
+++ Packages/jp.keijiro.bolt.minis/Runtime/MidiNoteUnit.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 namespace Bolt.Addons.Minis {
 
 [UnitCategory("MIDI"), UnitTitle("MIDI Note")]
-public sealed class MidiNoteUnit : Unit, IGraphElementWithData
+public sealed class MidiNoteUnit
+  : Unit, IGraphElementWithData, IGraphEventListener
 {
     #region Data class
 
     public sealed class Data : IGraphElementData
     {
         public MidiDevice Device { get; private set; }
+        public System.Action<EmptyEventArgs> UpdateAction { get; set; }
+        public float State { get; set; }
+        public float Velocity { get; set; }
 
         public bool CheckDevice(int channel)
         {
@@ -34,6 +38,15 @@ public sealed class MidiNoteUnit : Unit, IGraphElementWithData
     public ValueInput NoteNumber { get; private set; }
 
     [DoNotSerialize]
+    public ControlOutput NoteOn { get; private set; }
+
+    [DoNotSerialize]
+    public ControlOutput NoteOff { get; private set; }
+
+    [DoNotSerialize]
+    public ValueOutput Pressed { get; private set; }
+
+    [DoNotSerialize]
     public ValueOutput Velocity { get; private set; }
 
     #endregion
@@ -44,16 +57,70 @@ public sealed class MidiNoteUnit : Unit, IGraphElementWithData
     {
         Channel = ValueInput<int>(nameof(Channel), 0);
         NoteNumber = ValueInput<int>(nameof(NoteNumber), 0);
+		NoteOn = ControlOutput(nameof(NoteOn));
+		NoteOff = ControlOutput(nameof(NoteOff));
+        Pressed = ValueOutput<bool>(nameof(Pressed), IsPressed);
         Velocity = ValueOutput<float>(nameof(Velocity), GetVelocity);
     }
 
+    bool IsPressed(Flow flow)
+      => flow.stack.GetElementData<Data>(this).State > 0;
+
     float GetVelocity(Flow flow)
+      => flow.stack.GetElementData<Data>(this).Velocity;
+
+    #endregion
+
+    #region Graph event listener implementation
+
+    public void StartListening(GraphStack stack)
     {
-        var data = flow.stack.GetElementData<Data>(this);
-        var vChannel = flow.GetValue<int>(Channel);
-        if (!data.CheckDevice(vChannel)) return 0;
-        var vNoteNumber = flow.GetValue<int>(NoteNumber);
-        return data.Device.GetNote(vNoteNumber).velocity;
+        var data = stack.GetElementData<Data>(this);
+        if (data.UpdateAction != null) return;
+
+        var reference = stack.ToReference();
+        data.UpdateAction = args => OnUpdate(reference);
+
+        var hook = new EventHook(EventHooks.Update, stack.machine);
+        EventBus.Register(hook, data.UpdateAction);
+    }
+
+    public void StopListening(GraphStack stack)
+    {
+        var data = stack.GetElementData<Data>(this);
+        if (data.UpdateAction == null) return;
+
+        var hook = new EventHook(EventHooks.Update, stack.machine);
+        EventBus.Unregister(hook, data.UpdateAction);
+
+        data.UpdateAction = null;
+    }
+
+    public bool IsListening(GraphPointer pointer)
+      => pointer.GetElementData<Data>(this).UpdateAction != null;
+
+    #endregion
+
+    #region Update hook
+
+    void OnUpdate(GraphReference reference)
+    {
+        using (var flow = Flow.New(reference))
+        {
+            var data = flow.stack.GetElementData<Data>(this);
+
+            var vChannel = flow.GetValue<int>(Channel);
+            if (!data.CheckDevice(vChannel)) return;
+
+            var prev = data.State;
+
+            var vNoteNumber = flow.GetValue<int>(NoteNumber);
+            data.State = data.Device.GetNote(vNoteNumber).velocity;
+            if (data.State > 0) data.Velocity = data.State;
+
+            if (prev == 0 && data.State > 0) flow.Invoke(NoteOn);
+            if (prev > 0 && data.State == 0) flow.Invoke(NoteOff);
+        }
     }
 
     #endregion
